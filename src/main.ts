e598388779e3079017159560e0d695dc5e2a938d
James Coyle <jamescoyle1989@gmail.com>
2022-03-19 15:46:00 -0400

    The ts setup was proving to be a nightmare to get built and actually running

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import wrap from '@vue/web-component-wrapper';
-
-
-import MidiOutPicker from './components/MidiOutPicker.vue';
-const wrappedMidiOutPicker: any = wrap(Vue, MidiOutPicker);
-window.customElements.define('midi-out-picker', wrappedMidiOutPicker);
